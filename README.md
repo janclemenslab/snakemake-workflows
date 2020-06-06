a1316bf83b4effa26d5619a66e1f7e57daef179a
Jan Clemens <clemensjan@googlemail.com>
2020-06-07 00:04:10 +0200

    clean up

--- README.md
+++ README.md
@@ -1,5 +1,12 @@
-- Rules can use wrappers in `#Common/snakemake-workflows/RULENAME` (see [here](https://snakemake-wrappers.readthedocs.io/en/stable/index.html#contribute)):
-    - `wrapper.py`
-    - `environment.yaml` - conda environment for each rule (will be created automatically by snakemake and used for executing the rule. Snakemake will update the environment when the rule file changes)
+# Snakemake workflows
+For use as wrappers in snakefiles
+
+
+### How to make a new one
+See the repo and the official [snakemake-wrappers](https://snakemake-wrappers.readthedocs.io/en/stable/index.html#contribute) for examples.
+
+Each workflow contains:
+     - `wrapper.py` which creates the output for that rule from the inputs
+    - `environment.yaml` - conda environment for the wrapper. Will be created automatically by snakemake and used for executing the rule. Snakemake will update the environment when the rule file changes.
     - `meta.yaml` (optional)
-    - `models` or other folders for tf models etc.
+    - tf models or other stuff required by the job
--- deepss/wrapper.py
+++ deepss/wrapper.py
@@ -14,7 +14,9 @@ import flammkuchen
 
 # move to cli module
 def deepss(data_name: str, save_name: str, model_save_name: str, data_key: str = 'samples',
-        event_class_name: str = 'vibration', nb_channels: int = 16, event_thres: float = 0.5, event_tol: float = 0.01):
+           nb_channels: int = 16,
+           event_thres: float = 0.5, event_tol: float = 0.01,
+           segment_thres: float = 0.5):
     """[summary]
 
     Args:
@@ -23,8 +25,7 @@ def deepss(data_name: str, save_name: str, model_save_name: str, data_key: str =
         model_save_name (str): [description]
         data_key (str): [description]. Defaults to 'samples'.
         nb_channels (int): Number of channels to take from data file. Defaults to 16.
-        event_class_name (str): [description]. Defaults to 'pulse'.
-        event_thres (float): [description]. Defaults to 0.75.
+        event_thres (float): [description]. Defaults to 0.5.
         event_tol (float): [description]. Defaults to 0.01 seconds (10ms).
 
     Raises:
@@ -44,25 +45,17 @@ def deepss(data_name: str, save_name: str, model_save_name: str, data_key: str =
     sos_bp = ss.butter(5, [50, 1000], 'bandpass', output='sos', fs=samplerate)
     x = ss.sosfiltfilt(sos_bp, x, axis=0).astype(np.float16)
 
-    try:
-        event_index = params['class_names'].index(event_class_name)
-    except ValueError:
-        logging.info(f'model does not predict events of the type "{event_class_name}".')
-        event_index = None
-
     logging.info(f'   annotating {x.shape[0]/samplerate:1.2f} seconds')
     events, segments, class_probabilities = dss.predict.predict(x, model_save_name, params)
-    logging.info(f'   saving')
 
-    # save as xb
+
     logging.info(f'   saving results to "{save_name}".')
-
-    # TODO make predict work with multiple event types (list of event_index) - not just vibrations
     os.makedirs(os.path.dirname(save_name), exist_ok=True)
 
     event_names = [k for k in events.keys() if k != 'samplerate_Hz']
     segment_names = [k for k in segments.keys() if k != 'samplerate_Hz' and k != 'noise']
-    d = {'segment_names': segment_names,
+    d = {'class_probabilities': class_probabilities,
+        'segment_names': segment_names,
         'segment_probabilities':  [segments[segment_name]['probabilities'] for segment_name in segment_names],
         'segment_labels': [segments[segment_name]['samples'] for segment_name in segment_names],
         'event_names': event_names,
