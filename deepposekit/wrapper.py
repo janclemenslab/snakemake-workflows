faca9fbf32a18619a151ed5ae8472d22c33db6ba
Jan Clemens <clemensjan@googlemail.com>
2020-06-08 20:16:34 +0200

    fixes

--- deepposekit/wrapper.py
+++ deepposekit/wrapper.py
@@ -5,7 +5,7 @@ from videoreader import VideoReader
 import leap_utils as lu
 import xarray_behave as xb
 from leap_utils import preprocessing
-from leap_utils.utils import iswin, islinux, ismac
+# from leap_utils.utils import iswin, islinux, ismac
 from pathlib import Path
 import xarray as xr
 import pandas as pd
@@ -13,14 +13,14 @@ import logging
 import zarr
 # import dqefopt
 import os
-from tqdm import tqdm
+# from tqdm import tqdm
 
 
-def deepposekit(tracksfilename: str, savename:str, modelname: str = 'densenet-bg'):
+def deepposekit(tracksfilename: str, savename:str, modelname: str):
     datename = os.path.split(os.path.split(tracksfilename)[0])[1]
     root = os.getcwd()
 
-    model_filename = f'../snakemake-workflows/deepposekit/models/{modelname}_best_model.h5'
+    model_filename = modelname  #f'../snakemake-workflows/deepposekit/models/{modelname}'
     logging.info(f'using model from {model_filename}')
     model = load_model(model_filename)
 
@@ -39,7 +39,7 @@ def deepposekit(tracksfilename: str, savename:str, modelname: str = 'densenet-bg
     nb_frames = len(frame_numbers)
     logging.info(f'loading boxes from {nb_frames} frames.')
 
-    skeleton = pd.read_csv('snakemake-workflows/deepposekit/models/skeleton_initialized.csv')
+    skeleton = pd.read_csv(os.path.dirname(modelname) + '/skeleton_initialized.csv')
     body_parts_skeleton = skeleton['name'].tolist()
     logging.info('body parts from skeleton: {}.'.format(body_parts_skeleton))
     body_parts = ['head', 'neck', 'front_left_leg', 'middle_left_leg', 'back_left_leg', 'front_right_leg', 'middle_right_leg', 'back_right_leg', 'thorax', 'left_wing', 'right_wing', 'tail']
@@ -52,8 +52,6 @@ def deepposekit(tracksfilename: str, savename:str, modelname: str = 'densenet-bg
     batch_size = 100
     box_size = (64, 64) if 'scaled' in modelname else (96, 96)
 
-
-
     batch_idx = list(range(0, nb_frames, batch_size))
     nb_batches = len(batch_idx) - 1
     # batch_idx.append(int(frame_stop - frame_start - 2))
@@ -63,9 +61,15 @@ def deepposekit(tracksfilename: str, savename:str, modelname: str = 'densenet-bg
     poses_confidence = np.full((frame_stop, nb_flies, nb_parts, 1), np.nan)
     box_centers_from_batches = np.full((frame_stop, nb_flies, 2), np.nan)
     # box_centers_from_batches[:] = np.nan
-    pbar = tqdm(total=frame_stop, unit='frames')
+    # pbar = tqdm(total=frame_stop, unit='frames')
+    print('batch_idx', len(batch_idx))
+    print('nb_batches', nb_batches)
+    print(vr)
+    print('nb_frames (vr)', len(vr))
+    print('frame_numbers', len(frame_numbers))
+    print('frame_indices', len(frame_indices))
     for batch_num in range(nb_batches):
-        # logging.info(f"PROCESSING BATCH {batch_num:03d}/{nb_batches} (frames {frame_numbers[batch_idx[batch_num]]}-{frame_numbers[batch_idx[batch_num + 1]]}).")
+        logging.info(f"PROCESSING BATCH {batch_num:03d}/{nb_batches} (frames {frame_numbers[batch_idx[batch_num]]}-{frame_numbers[batch_idx[batch_num + 1]]}).")
         batch_frame_numbers = list(range(frame_numbers[batch_idx[batch_num]], frame_numbers[batch_idx[batch_num + 1]]))
         box_centers_batch = box_centers[batch_idx[batch_num]:batch_idx[batch_num + 1]]
 
@@ -91,8 +95,8 @@ def deepposekit(tracksfilename: str, savename:str, modelname: str = 'densenet-bg
             poses[frame, fly, ...] = pred
             poses_confidence[frame, fly, ...] = conf
             box_centers_from_batches[frame, fly, ...] = box_center
-        pbar.update(fly_frame[-1])
-    pbar.close()
+    #     pbar.update(fly_frame[-1])
+    # pbar.close()
     # convert to xarray and save to zarr
     poses = poses[..., ::-1]  # all data are in y/x - except for deepposekit - swap here to have order of coords like everywhere else
     poses = xr.DataArray(poses,
--- sleap/wrapper.py
+++ sleap/wrapper.py
@@ -7,5 +7,8 @@ extra.extend([f'--{k} {v} ' for k,v in params.items()])
 
 for out in snakemake.output:
     shell("umask g+rwx; \
-           sleap-track {snakemake.input.video} \
-           -output {out} {extra}")
+           module load cuda10.0/toolkit/10.0.130; \
+           module load cuda10.0/blas/10.0.130; \
+           module load cudnn/10.0v7.6.3; \
+           export CUDA_VISIBLE_DEVICES=0; \
+           sleap-track {snakemake.input.video} -output {out} {extra}")
